f3b80918a6968e572daf4a120d0b8cc89b9bf0ae
lauf8 <lucasafonsodesouza@gmail.com>
2023-02-20 18:12:22 -0300

    Ficha de avaliacao criada

---
+++ app/src/main/java/esperto/treino/Criar_Avaliacao.java
@@ -0,0 +1,37 @@
+package esperto.treino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Criar_Avaliacao extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_avaliacao);
+        Spinner spinner = findViewById(R.id.campo_genero_avaliacao);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.genero, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
+
--- app/src/main/java/esperto/treino/Detalhar_Aluno.java
+++ app/src/main/java/esperto/treino/Detalhar_Aluno.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Detalhar_Aluno extends AppCompatActivity {
 
+    Button criar_ficha;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhar_aluno);
         Intent it = getIntent();
         String id = it.getStringExtra("id");
+
+        criar_ficha = findViewById(R.id.btn_add_avaliacao_aluno);
+
+        criar_ficha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Detalhar_Aluno.this, Criar_Avaliacao.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
